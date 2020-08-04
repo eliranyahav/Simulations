eb3c670cc661c3b4299cba7d5fdf8c9a4e9fa107
Eliran Yahav <elirany22@gmail.com>
2020-08-05 00:19:20 +0300

    scales added. still few bugs with start button

--- collissions.py
+++ collissions.py
@@ -7,18 +7,18 @@ def draw_cir(x,y,r,color):
     cir=can.create_oval(x-r,y-r,x+r,y+r,fill=color)
     return cir
 def update_labels(x1,x2,m1,m2,v1,v2,space):
-    label_x1 = Label(root, text="x1=         ").place(x=20, y=space+100)
-    label_x1 = Label(root, text="x1= " + str("%.2f" %x1)).place(x=20, y=space+100)
-    label_x2 = Label(root, text="x2=         ").place(x=20, y=space*2+100)
-    label_x2 = Label(root, text="x2= " + str("%.2f" %x2)).place(x=20, y=space*2+100)
-    label_m1 = Label(root, text="m1=         ").place(x=20, y=space*3+100)
-    label_m1 = Label(root, text="m1= " + str("%.2f" %m1)).place(x=20, y=space*3+100)
-    label_m2 = Label(root, text="m2=         ").place(x=20, y=space*4+100)
-    label_m2 = Label(root, text="m2= " + str("%.2f" %m2)).place(x=20, y=space*4+100)
-    label_v1 = Label(root, text="v1=         ").place(x=20, y=space * 5 + 100)
-    label_v1 = Label(root, text="v1= " + str("%.2f" %v1)).place(x=20, y=space * 5 + 100)
-    label_v2 = Label(root, text="v2=         ").place(x=20, y=space * 6 + 100)
-    label_v2 = Label(root, text="v2= " + str("%.2f" %v2)).place(x=20, y=space * 6 + 100)
+    label_x1 = Label(root, text="x1=         ").place(x=20, y=100)
+    label_x1 = Label(root, text="x1= " + str("%.2f" %x1)).place(x=20, y=100)
+    label_x2 = Label(root, text="x2=         ").place(x=20, y=150)
+    label_x2 = Label(root, text="x2= " + str("%.2f" %x2)).place(x=20, y=150)
+    label_v1 = Label(root, text="v1=         ").place(x=20, y=280)
+    label_v1 = Label(root, text="v1= " + str("%.2f" % v1)).place(x=20, y=280)
+    label_v2 = Label(root, text="v2=         ").place(x=720, y=280)
+    label_v2 = Label(root, text="v2= " + str("%.2f" % v2)).place(x=720, y=280)
+    label_m1 = Label(root, text="m1=         ").place(x=20, y=380)
+    label_m1 = Label(root, text="m1= " + str("%.2f" % m1)).place(x=20, y=380)
+    label_m2 = Label(root, text="m2=         ").place(x=720, y=380)
+    label_m2 = Label(root, text="m2= " + str("%.2f" % m2)).place(x=720, y=380)
 #function which play the simulation
 def run():
     global i,index,cir,cir2,x1,x2,rad1,rad2,m1,m2,v1,v2,t,space,y
@@ -60,6 +60,8 @@ def start_clicked():
     elif i==True:
         i = False
         start_button['text'] = "continue"
+    update_labels(x1, x2, m1, m2, v1, v2, space)
+    set_clicked()
 
 #command when reset button clicked
 def reset_clicked():
@@ -90,6 +92,36 @@ def plastic_clicked():
     plastic=1
     plastic_button["bg"]='green'
     elastic_button["bg"] = 'red'
+def set_clicked():
+    global v1,v2,m1,m2,rad1,rad2
+    v1=var1.get()
+    v2=var2.get()
+    m1=var3.get()
+    m2=var4.get()
+    rad1=20*m1
+    rad2=20*m2
+    label_v1 = Label(root, text="v1=         ").place(x=20, y=280)
+    label_v1 = Label(root, text="v1= " + str("%.2f" % v1)).place(x=20, y=280)
+    label_v2 = Label(root, text="v2=         ").place(x=720, y=280)
+    label_v2 = Label(root, text="v2= " + str("%.2f" % v2)).place(x=720, y=280)
+    label_m1 = Label(root, text="m1=         ").place(x=20, y=380)
+    label_m1 = Label(root, text="m1= " + str("%.2f" % m1)).place(x=20, y=380)
+    label_m2 = Label(root, text="m2=         ").place(x=720, y=380)
+    label_m2 = Label(root, text="m2= " + str("%.2f" % m2)).place(x=720, y=380)
+def set_scales():
+    global var1,var2,var3,var4
+    var1 = DoubleVar()
+    var2 = DoubleVar()
+    var3 = DoubleVar()
+    var4 = DoubleVar()
+    scale1 = Scale(root, from_=-300, to=300, orient='horizontal', bg='blue', variable=var1).place(x=0, y=300)
+    scale2 = Scale(root, from_=-300, to=300, orient='horizontal', bg='blue', variable=var2).place(x=700, y=300)
+    scale3 = Scale(root, from_=1, to=18, orient='horizontal', bg='blue', variable=var3).place(x=0, y=400)
+    scale4 = Scale(root, from_=1, to=18, orient='horizontal', bg='blue', variable=var4).place(x=700, y=400)
+    set_button = Button(root, text="set", padx=50, bg='white', command=set_clicked).place(x=100, y=500)
+
+
+
 i= False    #global variable that tells if the simulation is running or not.
 index=0      # global variabels stand for time
 x01=100.
@@ -97,15 +129,16 @@ x02=500.
 x1=x01
 x2=x02
 y=300  #set the place of the balls on the screen
-rad1=20
-rad2=20
-v01=25.
-v02=-30.
+m1=1
+m2=1
+rad1=20*m1
+rad2=20*m2
+v01=0
+v02=0
 v1=v01
 v2=v02
 r=50
-m1=1
-m2=0.5
+
 t=0.05
 space=30 #vertical space between labels
 elastic=1
@@ -130,8 +163,6 @@ plastic_button=Button(root,text="plastic",padx=50,bg='red',command=plastic_click
 plastic_button.place(x=500,y=600)
 #set labels
 update_labels(x1, x2, m1, m2, v1, v2, space)
-
-'''w = Scale(from_=-100, to=100,orient='horizontal',bg='blue')
-w.place(x=100,y=400)'''
+set_scales()
 
 root.mainloop()
