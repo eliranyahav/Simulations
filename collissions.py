d69f9ea3729eb61053f703bb650261b046bf164a
Eliran Yahav <elirany22@gmail.com>
2020-08-05 09:55:30 +0300

    finished

--- collissions.py
+++ collissions.py
@@ -6,26 +6,28 @@ import time
 def draw_cir(x,y,r,color):
     cir=can.create_oval(x-r,y-r,x+r,y+r,fill=color)
     return cir
+###########################################################################
 def update_labels(x1,x2,m1,m2,v1,v2,space):
-    label_x1 = Label(root, text="x1=         ").place(x=20, y=100)
-    label_x1 = Label(root, text="x1= " + str("%.2f" %x1)).place(x=20, y=100)
-    label_x2 = Label(root, text="x2=         ").place(x=20, y=150)
-    label_x2 = Label(root, text="x2= " + str("%.2f" %x2)).place(x=20, y=150)
+    label_x1 = Label(root, text="x1=         ").place(x=20, y=250)
+    label_x1 = Label(root, text="x1= " + str("%.2f" %x1)+"   ").place(x=20, y=250)
+    label_x2 = Label(root, text="x2=         ").place(x=720, y=250)
+    label_x2 = Label(root, text="x2= " + str("%.2f" %x2)+"   ").place(x=720, y=250)
     label_v1 = Label(root, text="v1=         ").place(x=20, y=280)
-    label_v1 = Label(root, text="v1= " + str("%.2f" % v1)).place(x=20, y=280)
+    label_v1 = Label(root, text="v1= " + str("%.2f" % v1)+"   ").place(x=20, y=280)
     label_v2 = Label(root, text="v2=         ").place(x=720, y=280)
-    label_v2 = Label(root, text="v2= " + str("%.2f" % v2)).place(x=720, y=280)
+    label_v2 = Label(root, text="v2= " + str("%.2f" % v2)+"   ").place(x=720, y=280)
     label_m1 = Label(root, text="m1=         ").place(x=20, y=380)
-    label_m1 = Label(root, text="m1= " + str("%.2f" % m1)).place(x=20, y=380)
+    label_m1 = Label(root, text="m1= " + str("%.2f" % m1)+"   ").place(x=20, y=380)
     label_m2 = Label(root, text="m2=         ").place(x=720, y=380)
-    label_m2 = Label(root, text="m2= " + str("%.2f" % m2)).place(x=720, y=380)
+    label_m2 = Label(root, text="m2= " + str("%.2f" % m2)+"   ").place(x=720, y=380)
+#############################################################################
 #function which play the simulation
 def run():
     global i,index,cir,cir2,x1,x2,rad1,rad2,m1,m2,v1,v2,t,space,y
     global elastic,plastic
     can.delete(cir)
     can.delete(cir2)
-    cir = draw_cir(x1, y, rad1, 'red')
+    cir = draw_cir(x1, y, rad1, 'green')
     cir2 = draw_cir(x2, y, rad2, 'red')
     if i:
         can.delete(cir)
@@ -42,7 +44,7 @@ def run():
                  u=(m1*v1+m2*v2)/(m1+m2)
                  v1=u
                  v2=u
-        cir = draw_cir(x1,y, rad1, 'red')
+        cir = draw_cir(x1,y, rad1, 'green')
         cir2 = draw_cir(x2, y, rad2, 'red')
         #update labels
         update_labels(x1, x2, m1, m2, v1, v2, space)
@@ -50,8 +52,10 @@ def run():
         time.sleep(0.02)
         can.delete(cir)
         run()
+######################################################################
 #command when start button clicked
 def start_clicked():
+    set_clicked()
     global i
     if i==False:
         i = True
@@ -62,9 +66,10 @@ def start_clicked():
         start_button['text'] = "continue"
     update_labels(x1, x2, m1, m2, v1, v2, space)
     set_clicked()
-
+################################################################
 #command when reset button clicked
 def reset_clicked():
+    set_clicked()
     global index,space,y
     global i,x1,x2,v1,v2
     global cir,cir2
@@ -77,9 +82,9 @@ def reset_clicked():
     start_button['text'] = "start"
     can.delete(cir)
     can.delete(cir2)
-    cir = draw_cir(x1, y, rad1, 'red')
+    cir = draw_cir(x1, y, rad1, 'green')
     cir2 = draw_cir(x2,y, rad2, 'red')
-
+#######################################################################
 def elastic_clicked():
     global elastic,plastic
     elastic=1
@@ -92,14 +97,19 @@ def plastic_clicked():
     plastic=1
     plastic_button["bg"]='green'
     elastic_button["bg"] = 'red'
+#####################################################################
 def set_clicked():
-    global v1,v2,m1,m2,rad1,rad2
+    global v1,v2,m1,m2,rad1,rad2,cir,cir2
     v1=var1.get()
     v2=var2.get()
     m1=var3.get()
     m2=var4.get()
-    rad1=20*m1
-    rad2=20*m2
+    rad1 = 20 + 2 * m1
+    rad2 = 20 + 2 * m2
+    can.delete(cir)
+    can.delete(cir2)
+    cir = draw_cir(x1, y, rad1, 'green')
+    cir2 = draw_cir(x2, y, rad2, 'red')
     label_v1 = Label(root, text="v1=         ").place(x=20, y=280)
     label_v1 = Label(root, text="v1= " + str("%.2f" % v1)).place(x=20, y=280)
     label_v2 = Label(root, text="v2=         ").place(x=720, y=280)
@@ -108,31 +118,38 @@ def set_clicked():
     label_m1 = Label(root, text="m1= " + str("%.2f" % m1)).place(x=20, y=380)
     label_m2 = Label(root, text="m2=         ").place(x=720, y=380)
     label_m2 = Label(root, text="m2= " + str("%.2f" % m2)).place(x=720, y=380)
+#######################################################################################
 def set_scales():
     global var1,var2,var3,var4
     var1 = DoubleVar()
+    var1.set(30)
     var2 = DoubleVar()
+    var2.set(-30)
     var3 = DoubleVar()
     var4 = DoubleVar()
-    scale1 = Scale(root, from_=-300, to=300, orient='horizontal', bg='blue', variable=var1).place(x=0, y=300)
-    scale2 = Scale(root, from_=-300, to=300, orient='horizontal', bg='blue', variable=var2).place(x=700, y=300)
-    scale3 = Scale(root, from_=1, to=18, orient='horizontal', bg='blue', variable=var3).place(x=0, y=400)
-    scale4 = Scale(root, from_=1, to=18, orient='horizontal', bg='blue', variable=var4).place(x=700, y=400)
-    set_button = Button(root, text="set", padx=50, bg='white', command=set_clicked).place(x=100, y=500)
 
+    v1_scale = Scale(root, from_=-300, to=300, orient='horizontal', bg='blue', variable=var1).place(x=0, y=300)
+    v2_scale = Scale(root, from_=-300, to=300, orient='horizontal', bg='blue', variable=var2).place(x=700, y=300)
+    m1_scale = Scale(root, from_=1, to=18, orient='horizontal', bg='blue', variable=var3).place(x=0, y=400)
+    m2_scale = Scale(root, from_=1, to=18, orient='horizontal', bg='blue', variable=var4).place(x=700, y=400)
 
+    set_button = Button(root, text="set", padx=40, bg='yellow', command=set_clicked).place(x=0, y=500)
+    set_label=Label(root,text="Click set \nto update values")
+    set_label.config(font=("Courier", 8))
+    set_label.place(x=0,y=450)
+######main program##################################
 
 i= False    #global variable that tells if the simulation is running or not.
 index=0      # global variabels stand for time
-x01=100.
-x02=500.
+x01=200.
+x02=450.
 x1=x01
 x2=x02
 y=300  #set the place of the balls on the screen
 m1=1
 m2=1
-rad1=20*m1
-rad2=20*m2
+rad1=20+2*m1
+rad2=20+2*m2
 v01=0
 v02=0
 v1=v01
@@ -149,9 +166,9 @@ root.geometry("800x800")
 title=Label(root,text= "One dimensional collision",fg='green')
 title.config(font=("Courier", 30))
 title.pack()
-can=Canvas(root,width=600,height=600,background='black')
+can=Canvas(root,width=800,height=600,background='black')
 can.pack()
-cir = draw_cir(x1, y, rad1, 'red')
+cir = draw_cir(x1, y, rad1, 'green')
 cir2= draw_cir(x2, y, rad2, 'red')
 start_button=Button(root,text="start",padx=50,command=start_clicked)
 start_button.place(x=200,y=80)
@@ -161,6 +178,14 @@ elastic_button=Button(root,text="elastic",padx=50,bg='green' ,command=elastic_cl
 elastic_button.place(x=200,y=600)
 plastic_button=Button(root,text="plastic",padx=50,bg='red',command=plastic_clicked)
 plastic_button.place(x=500,y=600)
+
+ball1_label=Label(root,text="Ball 1",fg='green')
+ball1_label.config(font=("Courier", 15))
+ball1_label.place(x=20,y=200)
+
+ball2_label=Label(root,text="Ball 2",fg='red')
+ball2_label.config(font=("Courier", 15))
+ball2_label.place(x=720,y=200)
 #set labels
 update_labels(x1, x2, m1, m2, v1, v2, space)
 set_scales()
